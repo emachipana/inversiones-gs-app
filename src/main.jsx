5b411f5cf02ac9869f0c3c00a18e64a3bf8f156e
Enmanuel Chipana <emachipana0102@gmail.com>
2023-11-22 20:09:33 -0500

    IMPORT: bootstrap styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { Global } from '@emotion/react'
 import { RESET } from './styles/reset.js'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
