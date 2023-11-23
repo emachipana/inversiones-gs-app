82e468d989b343e21d7df6ce2d529fa6a4b7e8bf
Enmanuel Chipana <emachipana0102@gmail.com>
2023-11-22 20:04:14 -0500

    SET: global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Global } from '@emotion/react'
+import { RESET } from './styles/reset.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Global styles={RESET} />
     <App />
   </React.StrictMode>,
 )
