1f1f6d32418fc6f60c76d4840174f88b9dd5ff2f
Enmanuel Chipana <emachipana0102@gmail.com>
2023-11-22 20:06:06 -0500

    SET: browser router

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Global } from '@emotion/react'
 import { RESET } from './styles/reset.js'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Global styles={RESET} />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
