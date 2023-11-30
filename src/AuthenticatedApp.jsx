e09c3692d1d7817310334471939f7de223da50ab
Enmanuel Chipana <emachipana0102@gmail.com>
2023-11-30 12:09:18 -0500

    ADD: render pages

--- src/AuthenticatedApp.jsx
+++ src/AuthenticatedApp.jsx
@@ -1,6 +1,31 @@
+import Navbar from "./components/Navbar";
+import Aside from "./components/Aside";
+import { useState } from "react";
+import { useTheme } from "./context/theme";
+import { Container, Section } from "./styles";
+import { Route, Routes } from "react-router-dom";
+import Calc from "./pages/Calc";
+
 function AuthenticatedApp() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { theme } = useTheme();
+
   return (
-    <h1>Dashboard Page</h1>
+    <Container>
+      <Navbar 
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+      <Aside 
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+      <Section theme={theme}>
+        <Routes>
+          <Route path="/calculadora" element={<Calc />} />
+        </Routes>
+      </Section>
+    </Container>
   )
 }
 
--- src/UnauthenticatedApp.jsx
+++ src/UnauthenticatedApp.jsx
@@ -1,17 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuth } from "./context/auth";
 import Login from "./pages/Login";
 
 function UnauthenticatedApp() {
-  const { isLoading } = useAuth();
-
   return (
-    isLoading
-    ? "Cargando..."
-    : <Routes>
-        <Route index path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+    <Routes>
+      <Route index path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
