62fd250a567edfb7e1bfcd3d91cf9cadd95d9fa3
Enmanuel Chipana <emachipana0102@gmail.com>
2023-12-19 12:22:11 -0500

    ADD: profile page

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+import { COLORS } from "../../styles";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Form = styled.form`
+  width: 340px;
+  padding: 1rem;
+  border-radius: 1rem;
+  box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, .2);
+  background-color: ${({ theme }) => COLORS[theme].white};
+  margin: 0 0 2rem 0;
+`;
