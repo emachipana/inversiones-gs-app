3ccafef32bede78263520c267828b4594b83ad54
Enmanuel Chipana <emachipana0102@gmail.com>
2023-12-20 18:38:25 -0500

    ADD: refinancing page

--- src/pages/Loans/styles.js
+++ src/pages/Loans/styles.js
@@ -42,7 +42,7 @@ export const Section = styled.section`
   width: 100%;
   height: 100%;
   display: flex;
-  align-items: center;
+  align-items: ${({ align }) => align || "center"};
   justify-content: ${({ isLoading }) => isLoading ? "center" : "space-around"};
   flex-wrap: wrap;
   gap: 1rem;
